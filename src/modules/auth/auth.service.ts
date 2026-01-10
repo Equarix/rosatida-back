de55d96b6fbcdc937ca42a4d493eeed6831b6f39
laszlocaballero <laszlocaballero@gmail.com>
2026-01-09 23:23:50 -0500

    feat: implement OnModuleInit in AuthService to create default admin user

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './entity/User.schema';
@@ -8,11 +8,29 @@ import { JwtService } from '@nestjs/jwt';
 import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {
+export class AuthService implements OnModuleInit {
   constructor(
     @InjectModel(User.name) private userModel: Model<User>,
     private jwtService: JwtService,
   ) {}
+
+  async onModuleInit() {
+    const findUser = await this.userModel.findOne({ username: 'admin' }).exec();
+    if (!findUser) {
+      const hashedPassword = await hash('admin123', 10);
+      const newUser = await this.userModel.create({
+        username: 'admin',
+        password: hashedPassword,
+        role: 'admin',
+      });
+      await newUser.save();
+      console.log(
+        'Admin user created with username: admin and password: admin123',
+      );
+    }
+
+    console.log('AuthService initialized');
+  }
 
   async login(loginDto: LoginDto) {
     const { username, password } = loginDto;
